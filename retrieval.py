bc718e02b6611823f15aa53f266c6341486c5058
Nick Wogan <nicholaswogan@gmail.com>
2022-09-14 15:41:59 -0700

    adding proterozoic case

--- retrieval.py
+++ retrieval.py
@@ -5,15 +5,12 @@ import dynesty
 import rfast
 
 # initialize radiative transfer and retrieval
-r = rfast.Rfast('input/inputs.scr')
+r = rfast.Rfast('input/inputs_Proterozoic.scr')
 r.initialize_retrieval("input/rpars.txt")
 
-# Make fake data of Modern Earth twin.
 F1, F2 = r.genspec_scr()
 dat, err = r.noise(F2)
 
-# functions that need to be fed to nested sampler.
-# we pass data as globals.
 def lnlike(x_t):
     return rfast.lnlike_nest(x_t, r, dat, err)
 
@@ -22,7 +19,7 @@ def prior_transform(u):
         
 if __name__ == "__main__":
     # number of processes
-    NPROCESS = 4
+    NPROCESS = 48
     
     # retrieval with all gases
     with Pool(NPROCESS) as pool:
@@ -30,5 +27,5 @@ if __name__ == "__main__":
                                         nlive = 1000, pool=pool, queue_size=NPROCESS)
         sampler.run_nested(dlogz=0.001)
 
-    with open('normal_retrieval.pkl', 'wb') as f:
+    with open('results/Proterozoic_retrieval.pkl', 'wb') as f:
         pickle.dump(sampler.results, f)
--- utils.py
+++ utils.py
@@ -6,7 +6,10 @@ class FluxRetrieval():
     
     mxsteps = 50000
     equilibrium_time = 1e17
-    species = ['CO2','CO','CH4','H2']
+    burnin_redox_steps = 10000
+    check_redox_steps = 1000
+    redox_tolerance = 1e-5
+    species = ['CO2','CO','CH4','H2','O2']
     
     def __init__(self,a,b,c,d):
         self.pc = Atmosphere(a,b,c,d)
@@ -14,23 +17,51 @@ class FluxRetrieval():
     def equilibrium(self, usol):
         self.pc.initialize_stepper(usol)
         tn = 0.0
+        i = 0
         j = 0
         success = True
         try:
             while tn < self.equilibrium_time:
                 tn = self.pc.step()
                 j += 1
+                i += 1
+                if i > self.check_redox_steps and j > self.burnin_redox_steps:
+                    if self.pc.redox_conservation() < self.redox_tolerance:
+                        success = True
+                        break
+                    else:
+                        i = 0
                 if j > self.mxsteps:
-                    raise PhotoException('too many steps')
+                    if self.pc.redox_conservation() < self.redox_tolerance:
+                        success = True
+                        break
+                    else:
+                        raise PhotoException('too many steps')
         except PhotoException:
             success = False
         self.pc.destroy_stepper()
         
         return success
+
+    def column_average_mix(self):
+        # total column
+        dz = self.pc.var.z[1] - self.pc.var.z[0]
+        col = np.sum(self.pc.wrk.density*dz)
+
+        sol = self.pc.mole_fraction_dict()
+        out = {}
+        for sp in sol:
+            if sp in self.pc.dat.species_names:
+                sp_col = np.sum(self.pc.wrk.density*sol[sp]*dz)
+                mix = sp_col/col
+                out[sp] = mix
+
+        return out
     
     def equilibrium_result(self, success):
         fluxes = self.pc.gas_fluxes()[0]
         sol = self.pc.mole_fraction_dict()
+        mix = self.column_average_mix()
         
         out = {}
         out['fCO2'] = sol['CO2'][0]
@@ -49,6 +80,13 @@ class FluxRetrieval():
         out['FH2'] = fluxes['H2']
         out['vH2'] = -fluxes['H2']/(sol['H2'][0]*self.pc.wrk.density[0])
 
+        out['fO2'] = sol['O2'][0]
+        out['FO2'] = fluxes['O2']
+        out['vO2'] = -fluxes['O2']/(sol['O2'][0]*self.pc.wrk.density[0])
+
+        # column average O3 mixing ratio
+        out['cO3'] = mix['O3']
+
         if not success:
             for key in out:
                 out[key] = np.nan
@@ -57,11 +95,12 @@ class FluxRetrieval():
         
     def find_equilibrium(self, log10mix):
         mix = 10.0**log10mix
-        CO2, CO, CH4, H2 = mix
+        CO2, CO, CH4, H2, O2 = mix
         self.pc.set_lower_bc('CO2',bc_type='mix',mix = CO2)
         self.pc.set_lower_bc('CO',bc_type='mix',mix = CO)
         self.pc.set_lower_bc('CH4',bc_type='mix',mix = CH4)
         self.pc.set_lower_bc('H2',bc_type='mix',mix = H2)
+        self.pc.set_lower_bc('O2',bc_type='mix',mix = O2)
         
         # first try the correct answer
         success = self.equilibrium(self.pc.var.usol_init)
